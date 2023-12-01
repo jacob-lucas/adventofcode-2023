d44ec2fe19316942d85e070a56ee90c3e68ce658
jacoblucas <jacob.lucas@gmail.com>
2023-11-30 22:16:29 -0800

    Day 1 part 2

--- src/main/java/com/jacoblucas/adventofcode2023/day01/Day01.java
+++ src/main/java/com/jacoblucas/adventofcode2023/day01/Day01.java
@@ -3,18 +3,29 @@ package com.jacoblucas.adventofcode2023.day01;
 import com.jacoblucas.adventofcode2023.utils.InputReader;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.ToIntFunction;
 
 public class Day01 {
     public static void main(String[] args) throws IOException {
         final List<String> input = InputReader.read("day01-input.txt");
 
         // part 1
-        int calibrationValueSum = input.stream()
-                .mapToInt(Day01::getCalibrationValue)
+        int calibrationValueSumPart1 = getCalibrationValue(input, Day01::getCalibrationValue);
+        System.out.println(calibrationValueSumPart1);
+
+        // part 2
+        int calibrationValueSumPart2 = getCalibrationValue(input, Day01::getCalibrationValueV2);
+        System.out.println(calibrationValueSumPart2);
+    }
+
+    public static int getCalibrationValue(final List<String> input, final ToIntFunction<String> valueMapper) {
+        return input.stream()
+                .mapToInt(valueMapper)
                 .filter(i -> i > 0)
                 .sum();
-        System.out.println(calibrationValueSum);
     }
 
     public static int getCalibrationValue(final String string) {
@@ -39,5 +50,68 @@ public class Day01 {
         }
 
         return '\0';
+    }
+
+    public static int getCalibrationValueV2(final String string) {
+        if (string == null || string.isEmpty()) {
+            return -1;
+        }
+
+        int firstDigit = findDigit(string, 1);
+        if (firstDigit == -1) {
+            return -1;
+        }
+        int lastDigit = findDigit(string, 0);
+
+        System.out.println(string + ", first=" + firstDigit + ", last=" + lastDigit);
+
+        return Integer.parseInt("" + firstDigit + lastDigit);
+    }
+
+    // handles string representations of numbers as well as digits occurring within the string
+    // flag = 1 --> find first, flag = 0 --> find last
+    private static int findDigit(final String string, final int flag) {
+        final Map<String, Integer> searchSpace = new HashMap<>();
+        searchSpace.put("1", 1);
+        searchSpace.put("one", 1);
+        searchSpace.put("2", 2);
+        searchSpace.put("two", 2);
+        searchSpace.put("3", 3);
+        searchSpace.put("three", 3);
+        searchSpace.put("4", 4);
+        searchSpace.put("four", 4);
+        searchSpace.put("5", 5);
+        searchSpace.put("five", 5);
+        searchSpace.put("6", 6);
+        searchSpace.put("six", 6);
+        searchSpace.put("7", 7);
+        searchSpace.put("seven", 7);
+        searchSpace.put("8", 8);
+        searchSpace.put("eight", 8);
+        searchSpace.put("9", 9);
+        searchSpace.put("nine", 9);
+
+        int value = -1;
+        if (flag == 1) {
+            int earliest = Integer.MAX_VALUE;
+            for (Map.Entry<String, Integer> s : searchSpace.entrySet()) {
+                int i = string.indexOf(s.getKey());
+                if (i >= 0 && i < earliest) {
+                    earliest = i;
+                    value = s.getValue();
+                }
+            }
+        } else {
+            int latest = Integer.MIN_VALUE;
+            for (Map.Entry<String, Integer> s : searchSpace.entrySet()) {
+                int i = string.lastIndexOf(s.getKey());
+                if (i >= 0 && i > latest) {
+                    latest = i;
+                    value = s.getValue();
+                }
+            }
+        }
+
+        return value;
     }
 }
--- src/test/java/com/jacoblucas/adventofcode2023/day01/Day01Test.java
+++ src/test/java/com/jacoblucas/adventofcode2023/day01/Day01Test.java
@@ -1,6 +1,9 @@
 package com.jacoblucas.adventofcode2023.day01;
 
+import com.google.common.collect.ImmutableList;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -10,10 +13,19 @@ public class Day01Test {
     public void testNullString() {
         assertThat(Day01.getCalibrationValue(null), is(-1));
     }
+    @Test
+    public void testNullStringV2() {
+        assertThat(Day01.getCalibrationValueV2(null), is(-1));
+    }
 
     @Test
     public void testEmptyString() {
         assertThat(Day01.getCalibrationValue(""), is(-1));
+    }
+
+    @Test
+    public void testEmptyStringV2() {
+        assertThat(Day01.getCalibrationValueV2(""), is(-1));
     }
 
     @Test
@@ -22,8 +34,18 @@ public class Day01Test {
     }
 
     @Test
+    public void testStringWithoutNumbersV2() {
+        assertThat(Day01.getCalibrationValueV2("acbde"), is(-1));
+    }
+
+    @Test
     public void testStringOneNumber() {
         assertThat(Day01.getCalibrationValue("ac3bde"), is(33));
+    }
+
+    @Test
+    public void testStringOneNumberV2() {
+        assertThat(Day01.getCalibrationValueV2("acthreebde"), is(33));
     }
 
     @Test
@@ -32,7 +54,61 @@ public class Day01Test {
     }
 
     @Test
+    public void testStringTwoNumbersV2() {
+        assertThat(Day01.getCalibrationValueV2("acthreebdsevene"), is(37));
+    }
+
+    @Test
     public void testStringThreeNumbers() {
         assertThat(Day01.getCalibrationValue("ac9bbbbbbb5d0e"), is(90));
+    }
+
+    @Test
+    public void testStringThreeNumbersV2() {
+        assertThat(Day01.getCalibrationValueV2("acninebbbbbbbfivedtwoe"), is(92));
+    }
+
+    @Test
+    public void testPart2Examples() {
+        assertThat(Day01.getCalibrationValueV2("two1nine"), is(29));
+        assertThat(Day01.getCalibrationValueV2("eightwothree"), is(83));
+        assertThat(Day01.getCalibrationValueV2("abcone2threexyz"), is(13));
+        assertThat(Day01.getCalibrationValueV2("xtwone3four"), is(24));
+        assertThat(Day01.getCalibrationValueV2("4nineeightseven2"), is(42));
+        assertThat(Day01.getCalibrationValueV2("zoneight234"), is(14));
+        assertThat(Day01.getCalibrationValueV2("7pqrstsixteen"), is(76));
+    }
+
+    @Test
+    public void testRepeatedValues() {
+        assertThat(Day01.getCalibrationValue("9seveneightltwo59rnqh"), is(99));
+    }
+
+    @Test
+    public void testRepeatedValuesV2() {
+        assertThat(Day01.getCalibrationValueV2("9seveneightltwo59rnqh"), is(99));
+    }
+
+    @Test
+    public void testPart1Input() {
+        final List<String> input = ImmutableList.of(
+                "1abc2",
+                "pqr3stu8vwx",
+                "a1b2c3d4e5f",
+                "treb7uchet");
+        assertThat(Day01.getCalibrationValue(input, Day01::getCalibrationValue), is(142));
+    }
+
+    @Test
+    public void testPart2Input() {
+        final List<String> input = ImmutableList.of(
+                "two1nine",
+                "eightwothree",
+                "abcone2threexyz",
+                "xtwone3four",
+                "4nineeightseven2",
+                "zoneight234",
+                "7pqrstsixteen");
+        assertThat(Day01.getCalibrationValue(input, Day01::getCalibrationValueV2), is(281));
     }
 }
